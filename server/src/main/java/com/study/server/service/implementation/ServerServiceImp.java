22f09f1e4b67c2486d4aa68615f4f2c0bd6223c8
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-19 16:34:12 +0100

    Server Service Implementation.

---
+++ server/src/main/java/com/study/server/service/implementation/ServerServiceImp.java
@@ -0,0 +1,44 @@
+/**
+ * Server Service Implementation
+ */
+
+package com.study.server.service.implementation;
+
+import java.util.Collection;
+
+import com.study.server.model.Server;
+import com.study.server.service.ServerService;
+
+public class ServerServiceImp implements ServerService {
+
+    @Override
+    public Server create(Server server) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Collection<Server> list(int Limit) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Server get(Long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Server update(Server server) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
