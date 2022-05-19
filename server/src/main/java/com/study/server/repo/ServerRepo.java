52f6e5ebc82af11f37d0363d57a3901d105c0079
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-19 16:30:42 +0100

    Modify Server Repo.

--- server/src/main/java/com/study/server/repo/ServerRepo.java
+++ server/src/main/java/com/study/server/repo/ServerRepo.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ServerRepo extends JpaRepository<Server, Long> {
     Server findByIpAddress(String ipAddress);
+    
 }
