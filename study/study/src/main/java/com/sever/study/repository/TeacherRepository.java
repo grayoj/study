b15cc9e6e24ed9211f62ae312b57384c24cc769a
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-29 17:22:17 +0100

    Include Model and fix pending errors.

--- study/study/src/main/java/com/sever/study/repository/TeacherRepository.java
+++ study/study/src/main/java/com/sever/study/repository/TeacherRepository.java
@@ -1,5 +1,20 @@
+/**
+ * Gerald Maduabuchi
+ * Teacher repository
+ *
+ */
+
+ // Package
 package com.sever.study.repository;
 
+ // Import Teacher Model
+import com.sever.study.model.Teacher;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+// Import JpaRepository extension
+@Repository
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
     
 }
