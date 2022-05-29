a2f87f01caa1198ba969e246d9c8aca6ddda432f
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-29 15:18:50 +0100

    Create CRUD Functions.

--- study/study/src/main/java/com/sever/study/service/TeacherService.java
+++ study/study/src/main/java/com/sever/study/service/TeacherService.java
@@ -5,9 +5,18 @@
 // Import Package
 package com.sever.study.service;
 
+import java.util.List;
+import java.util.Optional;
+
 // Import Teacher Model
 import com.sever.study.model.Teacher;
 
+// Teacher Service API.
+
 public interface TeacherService {
-    Teacher includeTeacher(Teacher teacher)    
+    Teacher includeTeacher(Teacher teacher);
+    List<Teacher> findAllTeachers();
+    Optional<Teacher> findTeacherById(Long id);
+    Teacher updateTeacherById(Long id, Teacher teacher);
+    Teacher deleteTeacherById(Long id);    
 }
