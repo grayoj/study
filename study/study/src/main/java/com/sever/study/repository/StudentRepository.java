5969d8f5cc886c7b052f79a1757096fdce5f1c2b
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-29 17:38:08 +0100

    Comment and Format.

--- study/study/src/main/java/com/sever/study/repository/StudentRepository.java
+++ study/study/src/main/java/com/sever/study/repository/StudentRepository.java
@@ -1,13 +1,22 @@
+/**
+ * Gerald Maduabuchi
+ * Student repository
+ *
+ */
+
+// Package
+
 package com.sever.study.repository;
 
 import java.util.Optional;
 
+// Import Model
 import com.sever.study.model.Student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudentRepository extends JpaRepository <Student, Long> {
+public interface StudentRepository extends JpaRepository<Student, Long> {
     Optional<Student> getByEmail(String email);
 }
