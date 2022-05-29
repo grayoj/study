f9fad7c7282bb6e61ca366f8804be1ad12ae9854
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-29 15:24:12 +0100

    Import Subject Model.

--- study/study/src/main/java/com/sever/study/service/SubjectService.java
+++ study/study/src/main/java/com/sever/study/service/SubjectService.java
@@ -6,7 +6,11 @@
 // Include Service Package
 package com.sever.study.service;
 
+// Import Subject from Model
+import com.sever.study.model.Subject;
+
 // Create Public Interface and assign subject service to be implemented.
 public interface SubjectService {
+    Subject includeSubject(Subject subject);
     
 }
