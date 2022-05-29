73d486dd96167f618087092661d4b7b87f0094e7
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-29 15:20:13 +0100

    Include Comments for Service.

--- study/study/src/main/java/com/sever/study/service/TeacherService.java
+++ study/study/src/main/java/com/sever/study/service/TeacherService.java
@@ -14,9 +14,16 @@ import com.sever.study.model.Teacher;
 // Teacher Service API.
 
 public interface TeacherService {
+    // Function to include all teachers.
     Teacher includeTeacher(Teacher teacher);
+
+    // List and Fetch Teachers by ID.
     List<Teacher> findAllTeachers();
     Optional<Teacher> findTeacherById(Long id);
+
+    // Update Teacher by id
     Teacher updateTeacherById(Long id, Teacher teacher);
+
+    // Delete Teacher by id
     Teacher deleteTeacherById(Long id);    
 }
