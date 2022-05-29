4ebabd662ea29a9e16bfebaf45df317783f1794c
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-29 19:57:15 +0100

    DELETE Rest method.

--- study/study/src/main/java/com/sever/study/controller/SubjectController.java
+++ study/study/src/main/java/com/sever/study/controller/SubjectController.java
@@ -12,6 +12,7 @@ import com.sever.study.service.SubjectService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,5 +44,7 @@ public class SubjectController {
     }
 
     @DeleteMapping("/{id}")
-    
+    public Subject deleteSubjectById(@PathVariable Long id) {
+        return service.deleteSubjectById(id);
+    }
 }
