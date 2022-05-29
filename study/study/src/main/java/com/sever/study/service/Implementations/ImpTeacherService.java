6b1a79a6faa0800db40d9dd4ee88acbb35f670e8
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-29 18:13:07 +0100

    Create TeacherService Implementation.

---
+++ study/study/src/main/java/com/sever/study/service/Implementations/ImpTeacherService.java
@@ -0,0 +1,41 @@
+package com.sever.study.service.Implementations;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.sever.study.model.Student;
+import com.sever.study.service.StudentService;
+
+public class ImpTeacherService implements StudentService {
+
+    @Override
+    public Student includeStudent(Student student) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Optional<Student> getStudentById(Long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Student updateStudentById(Long id, Student student) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Student deleteStudentById(Long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
