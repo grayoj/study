6b5da72b56c2c7dc444c463429f355ad0832870d
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-29 19:48:51 +0100

    REST Controller.

---
+++ study/study/src/main/java/com/sever/study/controller/SubjectController.java
@@ -0,0 +1,17 @@
+/**
+ * Gerald Maduabuchi
+ * Subject Controller
+ * This Controller, implements the CRUD methods and REST 
+ */
+package com.sever.study.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/subjects")
+@CrossOrigin("*")
+public class SubjectController {
+    
+}
