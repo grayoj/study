66de0b24eec32943152eac64cc05d2546b2f5fd9
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-29 19:33:25 +0100

    Import Methods and Service.

--- study/study/src/main/java/com/sever/study/controller/StudentsController.java
+++ study/study/src/main/java/com/sever/study/controller/StudentsController.java
@@ -4,6 +4,9 @@
  */
 package com.sever.study.controller;
 
+import com.sever.study.service.StudentService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 // Import RestController Extensions.
@@ -15,5 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin("*")
 @RequestMapping("/students")
 public class StudentsController {
+    @Autowired
+    private StudentService service;
     
 }
