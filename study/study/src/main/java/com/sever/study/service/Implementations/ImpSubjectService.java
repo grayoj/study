62b40e8299af40bbc3c6677e32ecacc59778a3e5
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-29 18:48:54 +0100

    Fix.

--- study/study/src/main/java/com/sever/study/service/Implementations/ImpSubjectService.java
+++ study/study/src/main/java/com/sever/study/service/Implementations/ImpSubjectService.java
@@ -42,13 +42,13 @@ public class ImpSubjectService implements SubjectService {
     @Override
     public List<Subject> getAllSubjects() {
         // TODO Auto-generated method stub
-        return null;
+        return repository.findAll();
     }
 
     @Override
-    public Optional<Subject> getSubjectById() {
+    public Optional<Subject> getSubjectById(Long id) {
         // TODO Auto-generated method stub
-        return null;
+        return repository.findById(id);
     }
 
     @Override
@@ -65,6 +65,12 @@ public class ImpSubjectService implements SubjectService {
 
     @Override
     public Subject deleteSubjectById(Long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Optional<Subject> getSubjectById() {
         // TODO Auto-generated method stub
         return null;
     }
