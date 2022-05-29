0f2e1910bfb3d21ade1fd88458d7715162e71662
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-29 15:32:52 +0100

    Import Java Utilities.

--- study/study/src/main/java/com/sever/study/service/StudentService.java
+++ study/study/src/main/java/com/sever/study/service/StudentService.java
@@ -5,10 +5,16 @@
 // Import Package
 package com.sever.study.service;
 
+// Import Java Utilities.
+import java.util.List;
+import java.util.Optional;
+
 // Include Student Model.
 import com.sever.study.model.Student;
 
 public interface StudentService {
     Student includeStudent(Student student);
+    List<Student> getAllStudents();
+    Optional<Student> getStudentById(Long id);
     
 }
