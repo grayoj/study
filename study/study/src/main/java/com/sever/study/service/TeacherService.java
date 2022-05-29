7bf48c4a55f8071508c67b9e87114ef06c913908
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-29 15:15:55 +0100

    Import Teacher Model.

--- study/study/src/main/java/com/sever/study/service/TeacherService.java
+++ study/study/src/main/java/com/sever/study/service/TeacherService.java
@@ -5,7 +5,9 @@
 // Import Package
 package com.sever.study.service;
 
+// Import Teacher Model
+import com.sever.study.model.Teacher;
 
 public interface TeacherService {
-    
+    Teacher includeTeacher(Teacher teacher)    
 }
