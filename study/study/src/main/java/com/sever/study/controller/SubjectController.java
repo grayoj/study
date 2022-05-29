6d5d5c8aa5d5b3e224fdd805be9c83268a92ae23
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-29 19:55:37 +0100

    PUT Rest method.

--- study/study/src/main/java/com/sever/study/controller/SubjectController.java
+++ study/study/src/main/java/com/sever/study/controller/SubjectController.java
@@ -5,7 +5,18 @@
  */
 package com.sever.study.controller;
 
+import java.util.List;
+
+import com.sever.study.model.Subject;
+import com.sever.study.service.SubjectService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +24,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/subjects")
 @CrossOrigin("*")
 public class SubjectController {
+    @Autowired
+    private SubjectService service;
+
+    @PostMapping()
+    public Subject includeSubject(@RequestBody Subject subject) {
+        return service.includeSubject(subject);
+    }
+
+    @GetMapping()
+    public List<Subject> getSubjects() {
+        return service.getAllSubjects();
+    }
+
+    @PutMapping("/{id}")
+    public Subject updateSubjectById(@PathVariable Long id, @RequestBody Subject subject) {
+        return service.updateSubjectById(id, subject);
+    }
+
+    @DeleteMapping("/{id}")
     
 }
