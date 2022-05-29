dacfee36cc1ebab26fdb68989244cd1b53f3d373
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-29 15:11:17 +0100

    Create and Stage Teacher Model.

--- study/study/src/main/java/com/sever/study/model/Teacher.java
+++ study/study/src/main/java/com/sever/study/model/Teacher.java
@@ -1,5 +1,87 @@
+/**
+ * Gerald Maduabuchi
+ * Subject Model
+ */
+
+// Import Model
 package com.sever.study.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
 public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String qualifications;
+    @Column(unique = true)
+    private String email;
+    @JsonBackReference
+    @OneToMany(mappedBy = "teacher")
+    private List<Subject> subjects = new ArrayList<Subject>();
+
+    public Teacher() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getQualifications() {
+        return qualifications;
+    }
+
+    public void setQualifications(String qualifications) {
+        this.qualifications = qualifications;
+    }
+
+    public List<Subject> getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(List<Subject> subjects) {
+        this.subjects = subjects;
+    }
 
 }
