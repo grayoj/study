513c012f273e14493fe2298d06e6fa435120cb9d
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-29 19:38:44 +0100

    GET Rest API Function,

--- study/study/src/main/java/com/sever/study/controller/StudentsController.java
+++ study/study/src/main/java/com/sever/study/controller/StudentsController.java
@@ -12,6 +12,7 @@ import com.sever.study.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,5 +37,9 @@ public class StudentsController {
     public List<Student> getAllStudents() {
         return service.getAllStudents();
     }
-    
+
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable Long id) {
+        return service.getStudentById(id).get();
+    }
 }
