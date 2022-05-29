1400f96c5c59a8257456c083c41160ef9aa8e1ed
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-29 15:39:03 +0100

    Implement Override Methods.

---
+++ study/study/src/main/java/com/sever/study/service/Implementations/ImpTeacherService.java
@@ -0,0 +1,56 @@
+/**
+ * Gerald Maduabuchi
+ * Teacher Service Implementation
+ */
+
+// Include Service Package
+package com.sever.study.service.Implementations;
+
+// Import Utilities
+import java.util.List;
+import java.util.Optional;
+
+// Import Model
+import com.sever.study.model.Teacher;
+import com.sever.study.service.TeacherService;
+
+// Implement Service Function
+public class ImpTeacherService implements TeacherService {
+
+    @Override
+    public Teacher includeTeacher(Teacher teacher) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Teacher> getAllTeachers() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Optional<Teacher> getTeacherById(Long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Teacher updateTeacherById(Long id, Teacher teacher) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Teacher EditTeacherById(Long id, Teacher teacher) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Teacher deleteTeacherById(Long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
