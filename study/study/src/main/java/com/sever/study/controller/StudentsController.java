4db43bf1d65927d27299dbf92263988c9c2fad26
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-29 19:32:13 +0100

    Implement REST Controller.

---
+++ study/study/src/main/java/com/sever/study/controller/StudentsController.java
@@ -0,0 +1,19 @@
+/**
+ * Gerald Maduabuchi
+ * Student controller
+ */
+package com.sever.study.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+// Import RestController Extensions.
+import org.springframework.web.bind.annotation.RestController;
+
+// Controller Function
+@RestController
+// End point for Students mapping for API
+@CrossOrigin("*")
+@RequestMapping("/students")
+public class StudentsController {
+    
+}
