9c5bd2086d6197e7667790799a126289c3f68358
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-29 15:30:54 +0100

    Include Student Model

--- study/study/src/main/java/com/sever/study/service/StudentService.java
+++ study/study/src/main/java/com/sever/study/service/StudentService.java
@@ -5,6 +5,10 @@
 // Import Package
 package com.sever.study.service;
 
+// Include Student Package.
+import com.sever.study.model.Student;
+
 public interface StudentService {
+    Student includeStudent(Student student);
     
 }
