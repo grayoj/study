4c5206378774c4d98c07306760f8d73c093b27b5
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-29 15:23:11 +0100

    Stage Comments.

--- study/study/src/main/java/com/sever/study/service/SubjectService.java
+++ study/study/src/main/java/com/sever/study/service/SubjectService.java
@@ -1,5 +1,12 @@
+/**
+ * Gerald Maduabuchi
+ * Subject Model
+ */
+
+// Include Service Package
 package com.sever.study.service;
 
+// Create Public Interface and assign subject service to be implemented.
 public interface SubjectService {
     
 }
