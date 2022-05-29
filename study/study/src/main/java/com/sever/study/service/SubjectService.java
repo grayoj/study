37e5e838966c6c643c0ecc0420ad54ff33cf943c
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-29 15:27:30 +0100

    Create CRUD Functions. Switch to Get.

--- study/study/src/main/java/com/sever/study/service/SubjectService.java
+++ study/study/src/main/java/com/sever/study/service/SubjectService.java
@@ -6,11 +6,18 @@
 // Include Service Package
 package com.sever.study.service;
 
+import java.util.List;
+import java.util.Optional;
+
 // Import Subject from Model
 import com.sever.study.model.Subject;
 
 // Create Public Interface and assign subject service to be implemented.
 public interface SubjectService {
     Subject includeSubject(Subject subject);
-    
+    List<Subject> getAllSubjects();
+    Optional<Subject> getSubjectsById();
+    Optional<Subject> findAllSubjectById(Long id);
+    Subject updateSubjectById(Long id, Subject subject);
+    Subject deleteSubjectById(Long id);
 }
