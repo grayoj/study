0f862c59dd182f6547f77faec8ecd92062873605
Gerald Maduabuchi <Mgeraldoj07@gmail.com>
2022-05-29 18:15:40 +0100

    Call Autowired Method in Teacher Service Implementation.

--- study/study/src/main/java/com/sever/study/service/Implementations/ImpTeacherService.java
+++ study/study/src/main/java/com/sever/study/service/Implementations/ImpTeacherService.java
@@ -1,12 +1,31 @@
+/**
+ * Gerald Maduabuchi
+ * Teacher Service Implementation
+ */
 package com.sever.study.service.Implementations;
 
+// Java Service Utilities.
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
+// Import Models.
 import com.sever.study.model.Student;
+import com.sever.study.repository.StudentRepository;
 import com.sever.study.service.StudentService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+// Student Service Implementation
+
+@Service
+@Transactional
 public class ImpTeacherService implements StudentService {
+
+    @Autowired
+    private StudentRepository repository;
 
     @Override
     public Student includeStudent(Student student) {
